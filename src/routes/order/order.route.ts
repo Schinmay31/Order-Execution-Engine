8741419f7919d16fcf7c4503b837ef68f7b6bf1b
Schinmay31 <sabnischinmay31@gmail.com>
2025-11-22 13:03:27 +0530

    changes : routes folder structure

--- src/routes/order/order.route.ts
+++ src/routes/order/order.route.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify";
 
 export default async function orderRoutes(fastify: FastifyInstance) {
   fastify.post("/execute", async (request, reply) => {
+    fastify.redisClient.publish("order_updates", "Order executed");
     return { message: "execute OK" };
   });
 
